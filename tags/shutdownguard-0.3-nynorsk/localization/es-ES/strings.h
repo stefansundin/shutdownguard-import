38e6480959ba104bdd4621f174e17735e6f1c61e
recover89 <recover89@bb4cf866-5950-0410-8cd8-c3377ef70be7>
2009-01-17 13:26:44 +0000

    Adding and tagging Nynorsk translation to shutdownguard-0.3. Thanks Jon Stødle!

---
+++ tags/shutdownguard-0.3-nynorsk/localization/es-ES/strings.h
@@ -0,0 +1,46 @@
+/*
+	ShutdownGuard - es-ES localization by Fabrizio Ferri (algernon@gmail.com)
+	Do not localize APP_NAME, it will be automatically replaced.
+	Keep this file in UTF–8
+	
+	Copyright (C) 2008  Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+//This will be updated when the strings in the program changes
+#define L10N_VERSION 2
+
+//Strings
+#define L10N_MENU_ENABLE       L"Activar"
+#define L10N_MENU_DISABLE      L"Desactivar"
+#define L10N_MENU_HIDE         L"Ocultar icono"
+#define L10N_MENU_AUTOSTART    L"Iniciar con Windows"
+#define L10N_MENU_SHUTDOWN     L"Apagar"
+#define L10N_MENU_UPDATE       L"Actualización disponible"
+#define L10N_MENU_ABOUT        L"Acerca de"
+#define L10N_MENU_EXIT         L"Salir"
+#define L10N_TRAY_ENABLED      APP_NAME " (activado)"
+#define L10N_TRAY_DISABLED     APP_NAME " (desactivado)"
+#define L10N_PREVENT           L"Se bloqueó el intento de apagado."
+#define L10N_BALLOON           L"Haz clic para continuar el apagado."
+#define L10N_SHUTDOWN_ASK      L"Elige una opción"
+#define L10N_SHUTDOWN_LOGOFF   L"Cerrar sesión"
+#define L10N_SHUTDOWN_SHUTDOWN L"Apagar"
+#define L10N_SHUTDOWN_NOTHING  L"Nada"
+#define L10N_UPDATE_BALLOON    L"¡Nueva versión disponible!"
+#define L10N_UPDATE_DIALOG     L"Una nueva versión está disponible.\n¿Ir a la página de descarga?"
+#define L10N_ABOUT_TITLE       L"Acerca de " APP_NAME
+#define L10N_ABOUT             APP_NAME " - " APP_VERSION "\n\
+Programado por Stefan Sundin\n\
+Traducido por Fabrizio Ferri (algernon@gmail.com)\n\
+\n\
+Evita los apagados y reinicios indeseados de Windows.\n\
+Cuando ShutdownGuard bloquea un intento de apagado aparece un aviso en la barra de tareas preguntando si quieres continuar.\n\
+El clic central en el icono del área de notificación invocará el cuadro de opciones de apagado\n\
+\n\
+Visita mi página web en "APP_URL"\n\
+Envía tus comentarios recover89@gmail.com"
